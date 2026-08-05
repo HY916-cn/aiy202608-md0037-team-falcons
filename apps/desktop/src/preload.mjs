da31c4224d257426d4bb9e9a111f888c6368ec44
HY916 <huanghaoyu.0916@gmail.com>
2026-08-05 16:51:21 +0800

    chore(web): retire out-of-scope native delivery

--- apps/desktop/src/preload.mjs
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld(
-  "dolphinCloudDesktop",
-  Object.freeze({
-    getVersion: () => ipcRenderer.invoke("desktop:get-version"),
-  }),
-);
